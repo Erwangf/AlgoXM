cb79ce0025f073448e3d5eace6a7f25d4fc043dc
Franck <franck.11.h@gmail.com>
2016-12-20 03:08:38 +0100

    import, 1ere version avec gestion du format de date (pas encore dans une méthode)

--- src/main/java/controller/IOController.java
+++ src/main/java/controller/IOController.java
@@ -2,8 +2,19 @@ package controller;
 
 import model.Article;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import com.opencsv.CSVReader;
 
 /**
  * Controleur permettant l'import et l'export d'un ensemble d'articles
@@ -11,6 +22,41 @@ import java.util.ArrayList;
 public class IOController {
 
 
+	
+    public static void main(String[] args) throws ParseException {
+
+    	ArrayList<Article> artList = new ArrayList<Article>();
+    	Article art;
+        String csvFile = "E:/MASTER/JAVA/PROJET/data/10L_fr-blogs-1.csv";
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
+    /*	String s = "Mon, 20 Jun 2016 01:32:03 -0400";
+        Date formattedDate = simpleDateFormat.parse(s);
+        System.out.println("date : "+simpleDateFormat.format(formattedDate)); 
+     */
+        
+        CSVReader reader = null;
+        try {
+         //   reader = new CSVReader(new FileReader(csvFile),'\t', '\"', 1);
+            reader = new CSVReader(new InputStreamReader(new FileInputStream(csvFile), "UTF-8"),'\t', '\"', 1);
+           
+            
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+            	art = new Article(line[0], line[1], simpleDateFormat.parse(line[2]), line[3], line[4], new URL(line[5]));
+            	
+            	artList.add(art);
+            	
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(artList);
+    }
+	
+	
     /**
      * Importe un ensemble d'article depuis un InputReader fourni en paramètre.
      * @param fileIR Un InputReader associé à un fichier.
