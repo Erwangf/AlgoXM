5024189b66e67fe469d34b1ea812b8bf4d7267ce
Franck <franck.11.h@gmail.com>
2016-12-27 22:14:09 +0100

    Import CSV ok mais non commenté

--- src/main/java/controller/IOController.java
+++ src/main/java/controller/IOController.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -21,41 +22,66 @@ import com.opencsv.CSVReader;
  */
 public class IOController {
 
-
+	 SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
+	 Article art;
+	 ArrayList<Article> artList = new ArrayList<Article>();
+	 CSVReader reader = null;
+	 
+	 //test
+	 String csvFile = "E:/MASTER/JAVA/PROJET/data/10L_fr-blogs-1.csv";
+	 
+	 
 	
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
+    	
+    }
 
-    	ArrayList<Article> artList = new ArrayList<Article>();
-    	Article art;
-        String csvFile = "E:/MASTER/JAVA/PROJET/data/10L_fr-blogs-1.csv";
+    
+    public Date convertDate(String dateS) throws ParseException{
+    	return sdf.parse(dateS);
+    }
+    
+    
+    public Article readLine(String[] line) throws MalformedURLException, ParseException{
+    	art = new Article(line[0], line[1], convertDate(line[2]), line[3], line[4], new URL(line[5]));
+    
+    	return art;
+    }
+    	
+    
+    public ArrayList<Article> readFile(String path){
+    
+    	try {
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
+               reader = new CSVReader(new InputStreamReader(new FileInputStream(csvFile), "UTF-8"),'\t', '\"', 1);
+              
+               String[] fileLine;
+               while ((fileLine = reader.readNext()) != null) {
+               	artList.add(readLine(fileLine));
+                }
+           } catch (IOException e) {
+               e.printStackTrace();
+           } catch (ParseException e) {
+        	   e.printStackTrace();
+		}
+
+           return artList;
+    
+    }
+    	
+    	
+   // 	Article art;
+        
+
+     //   SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
     /*	String s = "Mon, 20 Jun 2016 01:32:03 -0400";
         Date formattedDate = simpleDateFormat.parse(s);
         System.out.println("date : "+simpleDateFormat.format(formattedDate)); 
      */
         
-        CSVReader reader = null;
-        try {
-         //   reader = new CSVReader(new FileReader(csvFile),'\t', '\"', 1);
-            reader = new CSVReader(new InputStreamReader(new FileInputStream(csvFile), "UTF-8"),'\t', '\"', 1);
-           
-            
-            String[] line;
-            while ((line = reader.readNext()) != null) {
-            	art = new Article(line[0], line[1], simpleDateFormat.parse(line[2]), line[3], line[4], new URL(line[5]));
-            	
-            	artList.add(art);
-            	
+        
+ 
 
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(artList);
-    }
-	
 	
     /**
      * Importe un ensemble d'article depuis un InputReader fourni en paramètre.
