62e2f89273a525e614f030c6acea3047bf937b04
Franck <franck.11.h@gmail.com>
2016-12-27 23:49:39 +0100

    Import + Export de CSV commentés

--- src/main/java/controller/IOController.java
+++ src/main/java/controller/IOController.java
@@ -2,11 +2,14 @@ package controller;
 
 import model.Article;
 
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
@@ -22,38 +25,52 @@ import com.opencsv.CSVReader;
  */
 public class IOController {
 
-	 SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
-	 Article art;
-	 ArrayList<Article> artList = new ArrayList<Article>();
-	 CSVReader reader = null;
+	
+	//*********PARTIE IMPORT*********//
+	
+		//format de la date, exemple : "Mon, 20 Jun 2016 01:32:03 -0400"
+		static SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
+		static Article art;
+		static ArrayList<Article> artList = new ArrayList<Article>();
+		static CSVReader reader = null;
 	 
-	 //test
-	 String csvFile = "E:/MASTER/JAVA/PROJET/data/10L_fr-blogs-1.csv";
+		//test
+		static String pathTest = "E:/MASTER/JAVA/PROJET/data/10L_fr-blogs-1.csv";
 	 
 	 
 	
-    public static void main(String[] args) {
-    	
-    }
-
-    
-    public Date convertDate(String dateS) throws ParseException{
+	/**
+     * Convertit une chaine de date en type date (ne fonctionne que pour le format "EEE, d MMM yyyy HH:mm:ss Z"
+     * @param dateS une date de type String
+     * @return dateS en type Date
+     */
+    public static Date convertDate(String dateS) throws ParseException{
     	return sdf.parse(dateS);
     }
     
     
-    public Article readLine(String[] line) throws MalformedURLException, ParseException{
+    /**
+     * Transforme un tableau de String (une ligne) passé en paramètre en un Article.
+     * @param line un tableau de String où chaque case = un champ d'un fichier
+     * @return art un objet Article
+     */
+    public static Article readLine(String[] line) throws MalformedURLException, ParseException{
     	art = new Article(line[0], line[1], convertDate(line[2]), line[3], line[4], new URL(line[5]));
     
     	return art;
     }
     	
     
-    public ArrayList<Article> readFile(String path){
+    /**
+     * Importe un ensemble d'article depuis un chemin de fichier fourni en paramètre.
+     * @param pathSrc un chemin de fichier csv (UTF-8, séparateur tabulation, 1 ligne d'entête, champs délimités par des ")
+     * @return ArrayList d'Articles
+     */
+    public static ArrayList<Article> readFile(String pathSrc){
     
     	try {
 
-               reader = new CSVReader(new InputStreamReader(new FileInputStream(csvFile), "UTF-8"),'\t', '\"', 1);
+               reader = new CSVReader(new InputStreamReader(new FileInputStream(pathSrc), "UTF-8"),'\t', '\"', 1);
               
                String[] fileLine;
                while ((fileLine = reader.readNext()) != null) {
@@ -69,26 +86,59 @@ public class IOController {
     
     }
     	
+    
+		//*********PARTIE EXPORT*********//
     	
-   // 	Article art;
-        
-
-     //   SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
-    /*	String s = "Mon, 20 Jun 2016 01:32:03 -0400";
-        Date formattedDate = simpleDateFormat.parse(s);
-        System.out.println("date : "+simpleDateFormat.format(formattedDate)); 
-     */
-        
-        
- 
-
+    
+    static String separator = "\t";
+    static String delimitor = "\"";
 	
+    
     /**
-     * Importe un ensemble d'article depuis un InputReader fourni en paramÃ¨tre.
-     * @param fileIR Un InputReader associÃ© Ã  un fichier.
-     * @return ArrayList d'Articles
+     * Transforme un article en String contenant les données, le délimiteur et le séparateur
+     * @param a un article
+     * @return oneLine un String contenant une ligne article
      */
-    public static ArrayList<Article> importCSV(InputStream fileIR) {
-        return null;
+	public static String artToString(Article a){
+		String oneLine = delimitor + a.getTitle() + delimitor + separator + delimitor + a.getDescription() + delimitor + separator
+				+ delimitor + sdf.format(a.getDate()) + delimitor + separator + delimitor + a.getRss() + delimitor + separator
+				+ delimitor + a.getAuthor() + delimitor + separator + delimitor + a.getLink() + delimitor;
+		return oneLine;
+	}
+    
+	
+	/**
+     * Crée une ligne String pour chaque article d'une ArrayList et l'ajoute au fichier csv passé en paramètre
+     * @param listArt une liste d'articles, pathTgt un chemin cible pour un fichier csv
+     */
+	public static void writeToCSV(ArrayList<Article> listArt, String pathTgt) {
+		try {
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(pathTgt), "UTF-8"));
+			for (Article a : listArt) {
+				bw.write(artToString(a));
+				bw.newLine();
+			}
+			bw.flush();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();			
+		}
+	}
+    
+    
+    
+    public static void main(String[] args) {
+    	// System.out.println(readFile(pathTest));
+    	writeToCSV(readFile(pathTest), "E:/MASTER/JAVA/PROJET/data/test1.csv");
     }
+ 
+        
+    /*	test format de date
+     
+      String s = "Mon, 20 Jun 2016 01:32:03 -0400";
+      Date formattedDate = sdf.parse(s);
+      System.out.println("date : "+sdf.format(formattedDate)); 
+     */
+          
+
 }
