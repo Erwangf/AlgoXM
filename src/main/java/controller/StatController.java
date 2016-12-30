99eb446d426be9e66a40d3d050b42ddaeb10597a
Franck <franck.11.h@gmail.com>
2016-12-30 19:14:07 +0100

    Ajout des premières stats : nb de docs indexés, nb de mots moyen/doc

--- src/main/java/controller/StatController.java
+++ src/main/java/controller/StatController.java
@@ -1,8 +1,30 @@
 package controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.ParseException;
+
+import model.Article;
+
 
 /**
  * Contrôleur permettant d'afficher différentes statistiques d'une ArrayList d'Article avec Lucene
  */
 public class StatController {
+	
+	
+	public static void main(String[]args) throws IOException, ParseException{
+		
+		ArticleIndex AI = new ArticleIndex();
+        ArrayList<Article> list = IOController.readFile("E:/MASTER/JAVA/PROJET/data/10L_fr-blogs-1.csv");
+        AI.addArticles(list);
+        System.out.println("Articles ajoutés !");
+        
+        //System.out.println(AI.getNbDocTerm(new Term("description", "Paris")) );
+       // AI.readingIndex();
+        
+	}
+	
 }
