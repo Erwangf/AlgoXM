ff8c49c1180fc0789d2a3cbd3b9bff27aa57848b
ErwanGF <erwangf@gmail.com>
2017-01-04 10:08:11 +0100

    Colonnes non cliquables

--- src/main/java/view/component/ArticleListPane.java
+++ src/main/java/view/component/ArticleListPane.java
@@ -3,12 +3,10 @@ package view.component;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
-import javafx.util.Callback;
 import model.Article;
 
 import java.util.ArrayList;
@@ -22,8 +20,7 @@ public class ArticleListPane extends GridPane {
     public ArticleListPane() {
 
 
-        Callback<TableColumn<Article, String>, TableCell<Article, String>> stringCellFactory =
-                p -> new MyStringTableCell();
+
 
 
 
@@ -35,41 +32,24 @@ public class ArticleListPane extends GridPane {
         titleColumn.setSortable(false);
         TableColumn<Article, String> descriptionColumn = new TableColumn<>("Description");
         descriptionColumn.setCellValueFactory((p -> new SimpleStringProperty(p.getValue().getDescription().substring(0, 40))));
-        descriptionColumn.setCellFactory(stringCellFactory);
+
         descriptionColumn.setSortable(false);
         TableColumn<Article, String> authorColumn = new TableColumn<>("Auteur");
         authorColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getAuthor()));
-        authorColumn.setCellFactory(stringCellFactory);
         authorColumn.setSortable(false);
 
         TableColumn<Article, String> dateColumn = new TableColumn<>("Date");
         dateColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getStrDate()));
-        dateColumn.setCellFactory(stringCellFactory);
         dateColumn.setSortable(false);
-//        dateColumn.setComparator((a,b)->{
-//            try {
-//                Date _a = IOController.convertDate(a);
-//                Date _b = IOController.convertDate(b);
-//                return _a.compareTo(_b);
-//            } catch (ParseException e) {
-//                e.printStackTrace();
-//                return 0;
-//            }
-//        });
-
 
         TableColumn<Article, String> rssColumn = new TableColumn<>("RSS");
         rssColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getRss()));
-        rssColumn.setCellFactory(stringCellFactory);
         rssColumn.setSortable(false);
 
 
         TableColumn<Article, String> linkColumn = new TableColumn<>("Lien");
         linkColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getLink().toString()));
-        linkColumn.setCellFactory(stringCellFactory);
         linkColumn.setSortable(false);
-
-
 
         tableView.getColumns().addAll(
                 titleColumn,
@@ -104,17 +84,5 @@ public class ArticleListPane extends GridPane {
 
 
 
-    class MyStringTableCell extends TableCell<Article, String> {
 
-        @Override
-        public void updateItem(String item, boolean empty) {
-            super.updateItem(item, empty);
-            setText(empty ? null : getString());
-            //setGraphic(null);
-        }
-
-        private String getString() {
-            return getItem() == null ? "" : getItem();
-        }
-    }
 }
